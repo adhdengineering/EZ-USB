edda750a48a71a18c8ad01c9dc07a4bd7fa6aa0e
adhdengineer <adhdengineer@gmail.com>
2016-05-31 11:17:18 +0100

    Adding the USBVendorCommand project (uses libCore) Also the way the USB setup packet handler was dealing with Vendor requests was... wrong. The default cypress code did all the setup stuff then if it hadnt handled it, it assumed it was a Vendor Request rather than checking setupdata[0] and seeing what type of command it was.

---
+++ USBVendorCommand/main.c
@@ -0,0 +1,92 @@
+#include <Fx2.h>
+#include <fx2regs.h>
+#include <syncdly.h>            // SYNCDELAY macro
+
+#include <USB.h>
+#include <delay.h>
+#include <USBsetup.h>
+#include <SuspendResume.h>
+
+#include "LEDs.h"
+#include "i2c.h"
+
+void ProcessEP1Out()
+{
+	WORD i;
+
+	SetLEDState(0, EP1OUTBUF[0]);
+	SetLEDState(1, EP1OUTBUF[1]);
+	SetLEDState(2, EP1OUTBUF[2]);
+	SetLEDState(3, EP1OUTBUF[3]);
+
+	for (i = 0; i < EP1OUTBC; i++)
+	{
+		EP1INBUF[i] = ~EP1OUTBUF[i];
+	}
+	EP1INBC = EP1OUTBC;
+	EP1OUTBC = 0; // rearm the endpoint out buffer
+
+}
+
+BOOL TD_Suspend(void) // Called before the device goes into suspend mode
+{
+	return (TRUE);
+}
+
+BOOL TD_Resume(void) // Called after the device resumes
+{
+	return (TRUE);
+}
+
+// I'd like to move main into the core library, but it has to remain in the target project
+// for now. I can't get the linker to include it in the output if it's in the library.
+// Same as the USBJumpTable.asm file.
+void main(void)
+{
+	InitUSB();
+	SetupLEDs();
+
+	if (i2c_print_string(0x8, "Testing\n") == 1)
+	{
+	    SetLEDState(0, 1);
+	    SetLEDState(1, 1);
+	}
+	else {
+	    SetLEDState(0, 0);
+	    SetLEDState(1, 0);
+	}
+
+	while (1)
+	{
+		PollUSB();
+
+		// check for and handle suspend.
+		// NOTE: Idle mode stops the processor clock.  There are only two
+		// ways out of idle mode, the WAKEUP pin, and detection of the USB
+		// resume state on the USB bus.  The timers will stop and the
+		// processor will not wake up on any other interrupts.
+		if (Sleep)
+		{
+			if(TD_Suspend())
+			{
+				Sleep = FALSE;     // Clear the "go to sleep" flag.  Do it here to prevent any race condition between wakeup and the next sleep.
+				do
+				{
+					Suspend();         // Place processor in idle mode.
+				}while(!Rwuen && EZUSB_EXTWAKEUP());
+				// above.  Must continue to go back into suspend if the host has disabled remote wakeup
+				// *and* the wakeup was caused by the external wakeup pin.
+
+				// 8051 activity will resume here due to USB bus or Wakeup# pin activity.
+				Resume();   // If source is the Wakeup# pin, signal the host to Resume.
+				TD_Resume();
+			}
+		}
+	}
+}
+
+// Wake-up interrupt handler
+void resume_isr(void) __interrupt(WKUP_VECT)
+{
+   EZUSB_CLEAR_RSMIRQ();
+}
--- libCore/USBsetup.c
+++ libCore/USBsetup.c
@@ -3,6 +3,7 @@
 #include "syncdly.h"            // SYNCDELAY macro
 
 #include "USBDescriptors.h"
+#include "i2c.h"
 
 extern BOOL Rwuen;
 extern BOOL Selfpwr;
@@ -54,143 +55,157 @@ extern BOOL DR_ClearFeature(void);
 extern BOOL DR_SetFeature(void);
 extern BOOL DR_VendorCmnd(void);
 
+void HandleSetupPacket();
 void SetupCommand(void)
 {
-   void   *dscr_ptr;
-
-   switch(SETUPDAT[1])
+   if ((SETUPDAT[0] & SETUP_VENDOR_REQUEST) == SETUP_VENDOR_REQUEST)
    {
-      case SC_GET_DESCRIPTOR:                  // *** Get Descriptor
-         if(DR_GetDescriptor())
-            switch(SETUPDAT[3])
-            {
-               case GD_DEVICE:            // Device
-                  SUDPTRH = MSB(pDeviceDscr);
-                  SUDPTRL = LSB(pDeviceDscr);
-                  break;
-               case GD_DEVICE_QUALIFIER:            // Device Qualifier
-			   	  // only retuen a device qualifier if this is a high speed
-				  // capable chip.
-			   	  if (HighSpeedCapable())
-				  {
-	                  SUDPTRH = MSB(pDeviceQualDscr);
-	                  SUDPTRL = LSB(pDeviceQualDscr);
-				  }
-				  else
-				  {
-					  EZUSB_STALL_EP0();
-				  }
-				  break;
-               case GD_CONFIGURATION:         // Configuration
-                  SUDPTRH = MSB(pConfigDscr);
-                  SUDPTRL = LSB(pConfigDscr);
-                  break;
-               case GD_OTHER_SPEED_CONFIGURATION:  // Other Speed Configuration
-                  SUDPTRH = MSB(pOtherConfigDscr);
-                  SUDPTRL = LSB(pOtherConfigDscr);
-                  break;
-               case GD_STRING:            // String
-                  if( (dscr_ptr = GetStringDescriptor(SETUPDAT[2])) != 0)
-                  {
-                     SUDPTRH = MSB(dscr_ptr);
-                     SUDPTRL = LSB(dscr_ptr);
-                  }
-                  else
-                     EZUSB_STALL_EP0();   // Stall End Point 0
-                  break;
-               default:            // Invalid request
-                  EZUSB_STALL_EP0();      // Stall End Point 0
-            }
-         break;
-      case SC_GET_INTERFACE:                  // *** Get Interface
-         DR_GetInterface();
-         break;
-      case SC_SET_INTERFACE:                  // *** Set Interface
-         DR_SetInterface();
-         break;
-      case SC_SET_CONFIGURATION:               // *** Set Configuration
-         DR_SetConfiguration();
-         break;
-      case SC_GET_CONFIGURATION:               // *** Get Configuration
-         DR_GetConfiguration();
-         break;
-      case SC_GET_STATUS:                  // *** Get Status
-         if(DR_GetStatus())
-            switch(SETUPDAT[0])
-            {
-               case GS_DEVICE:            // Device
-                  EP0BUF[0] = ((BYTE)Rwuen << 1) | (BYTE)Selfpwr;
-                  EP0BUF[1] = 0;
-                  EP0BCH = 0;
-                  EP0BCL = 2;
-                  break;
-               case GS_INTERFACE:         // Interface
-                  EP0BUF[0] = 0;
-                  EP0BUF[1] = 0;
-                  EP0BCH = 0;
-                  EP0BCL = 2;
-                  break;
-               case GS_ENDPOINT:         // End Point
-                  EP0BUF[0] = *(BYTE __xdata *) epcs(SETUPDAT[4]) & bmEPSTALL;
-                  EP0BUF[1] = 0;
-                  EP0BCH = 0;
-                  EP0BCL = 2;
-                  break;
-               default:            // Invalid Command
-                  EZUSB_STALL_EP0();      // Stall End Point 0
-            }
-         break;
-      case SC_CLEAR_FEATURE:                  // *** Clear Feature
-         if(DR_ClearFeature())
-            switch(SETUPDAT[0])
-            {
-               case FT_DEVICE:            // Device
-                  if(SETUPDAT[2] == 1)
-                     Rwuen = FALSE;       // Disable Remote Wakeup
-                  else
-                     EZUSB_STALL_EP0();   // Stall End Point 0
-                  break;
-               case FT_ENDPOINT:         // End Point
-                  if(SETUPDAT[2] == 0)
-                  {
-                     *(BYTE __xdata *) epcs(SETUPDAT[4]) &= ~bmEPSTALL;
-                     EZUSB_RESET_DATA_TOGGLE( SETUPDAT[4] );
-                  }
-                  else
-                     EZUSB_STALL_EP0();   // Stall End Point 0
-                  break;
-            }
-         break;
-      case SC_SET_FEATURE:                  // *** Set Feature
-         if(DR_SetFeature())
-            switch(SETUPDAT[0])
-            {
-               case FT_DEVICE:            // Device
-                  if(SETUPDAT[2] == 1)
-                     Rwuen = TRUE;      // Enable Remote Wakeup
-                  else if(SETUPDAT[2] == 2)
-                     // Set Feature Test Mode.  The core handles this request.  However, it is
-                     // necessary for the firmware to complete the handshake phase of the
-                     // control transfer before the chip will enter test mode.  It is also
-                     // necessary for FX2 to be physically disconnected (D+ and D-)
-                     // from the host before it will enter test mode.
-                     break;
-                  else
-                     EZUSB_STALL_EP0();   // Stall End Point 0
-                  break;
-               case FT_ENDPOINT:         // End Point
-                  *(BYTE __xdata *) epcs(SETUPDAT[4]) |= bmEPSTALL;
-                  break;
-               default:
-                  EZUSB_STALL_EP0();      // Stall End Point 0
-            }
-         break;
-      default:                     // *** Invalid Command
-         if(DR_VendorCmnd())
-            EZUSB_STALL_EP0();            // Stall End Point 0
-         break;
+       if(DR_VendorCmnd()){
+          EZUSB_STALL_EP0();            // Stall End Point 0
+       }
    }
-
+   else
+   {
+       HandleSetupPacket();
+   }
    // Acknowledge handshake phase of device request
    EP0CS |= bmHSNAK;
+}
+
+void HandleSetupPacket()
+{
+    void   *dscr_ptr;
+    switch(SETUPDAT[1])
+    {
+       case SC_GET_DESCRIPTOR:                  // *** Get Descriptor
+          if(DR_GetDescriptor())
+             switch(SETUPDAT[3])
+             {
+                case GD_DEVICE:            // Device
+                   SUDPTRH = MSB(pDeviceDscr);
+                   SUDPTRL = LSB(pDeviceDscr);
+                   break;
+                case GD_DEVICE_QUALIFIER:            // Device Qualifier
+                   // only retuen a device qualifier if this is a high speed
+                   // capable chip.
+                   if (HighSpeedCapable())
+                   {
+                       SUDPTRH = MSB(pDeviceQualDscr);
+                       SUDPTRL = LSB(pDeviceQualDscr);
+                   }
+                   else
+                   {
+                       EZUSB_STALL_EP0();
+                   }
+                   break;
+                case GD_CONFIGURATION:         // Configuration
+                   SUDPTRH = MSB(pConfigDscr);
+                   SUDPTRL = LSB(pConfigDscr);
+                   break;
+                case GD_OTHER_SPEED_CONFIGURATION:  // Other Speed Configuration
+                   SUDPTRH = MSB(pOtherConfigDscr);
+                   SUDPTRL = LSB(pOtherConfigDscr);
+                   break;
+                case GD_STRING:            // String
+                   if( (dscr_ptr = GetStringDescriptor(SETUPDAT[2])) != 0)
+                   {
+                      SUDPTRH = MSB(dscr_ptr);
+                      SUDPTRL = LSB(dscr_ptr);
+                   }
+                   else
+                      EZUSB_STALL_EP0();   // Stall End Point 0
+                   break;
+                default:            // Invalid request
+                   EZUSB_STALL_EP0();      // Stall End Point 0
+             }
+          break;
+       case SC_GET_INTERFACE:                  // *** Get Interface
+          DR_GetInterface();
+          break;
+       case SC_SET_INTERFACE:                  // *** Set Interface
+          DR_SetInterface();
+          break;
+       case SC_SET_CONFIGURATION:               // *** Set Configuration
+          DR_SetConfiguration();
+          break;
+       case SC_GET_CONFIGURATION:               // *** Get Configuration
+          DR_GetConfiguration();
+          break;
+       case SC_GET_STATUS:                  // *** Get Status
+          if(DR_GetStatus())
+             switch(SETUPDAT[0])
+             {
+                case GS_DEVICE:            // Device
+                   EP0BUF[0] = ((BYTE)Rwuen << 1) | (BYTE)Selfpwr;
+                   EP0BUF[1] = 0;
+                   EP0BCH = 0;
+                   EP0BCL = 2;
+                   break;
+                case GS_INTERFACE:         // Interface
+                   EP0BUF[0] = 0;
+                   EP0BUF[1] = 0;
+                   EP0BCH = 0;
+                   EP0BCL = 2;
+                   break;
+                case GS_ENDPOINT:         // End Point
+                   EP0BUF[0] = *(BYTE __xdata *) epcs(SETUPDAT[4]) & bmEPSTALL;
+                   EP0BUF[1] = 0;
+                   EP0BCH = 0;
+                   EP0BCL = 2;
+                   break;
+                default:            // Invalid Command
+                   EZUSB_STALL_EP0();      // Stall End Point 0
+             }
+          break;
+       case SC_CLEAR_FEATURE:                  // *** Clear Feature
+          if(DR_ClearFeature())
+             switch(SETUPDAT[0])
+             {
+                case FT_DEVICE:            // Device
+                   if(SETUPDAT[2] == 1)
+                      Rwuen = FALSE;       // Disable Remote Wakeup
+                   else
+                      EZUSB_STALL_EP0();   // Stall End Point 0
+                   break;
+                case FT_ENDPOINT:         // End Point
+                   if(SETUPDAT[2] == 0)
+                   {
+                      *(BYTE __xdata *) epcs(SETUPDAT[4]) &= ~bmEPSTALL;
+                      EZUSB_RESET_DATA_TOGGLE( SETUPDAT[4] );
+                   }
+                   else
+                      EZUSB_STALL_EP0();   // Stall End Point 0
+                   break;
+             }
+          break;
+       case SC_SET_FEATURE:                  // *** Set Feature
+          if(DR_SetFeature())
+             switch(SETUPDAT[0])
+             {
+                case FT_DEVICE:            // Device
+                   if(SETUPDAT[2] == 1)
+                      Rwuen = TRUE;      // Enable Remote Wakeup
+                   else if(SETUPDAT[2] == 2)
+                      // Set Feature Test Mode.  The core handles this request.  However, it is
+                      // necessary for the firmware to complete the handshake phase of the
+                      // control transfer before the chip will enter test mode.  It is also
+                      // necessary for FX2 to be physically disconnected (D+ and D-)
+                      // from the host before it will enter test mode.
+                      break;
+                   else
+                      EZUSB_STALL_EP0();   // Stall End Point 0
+                   break;
+                case FT_ENDPOINT:         // End Point
+                   *(BYTE __xdata *) epcs(SETUPDAT[4]) |= bmEPSTALL;
+                   break;
+                default:
+                   EZUSB_STALL_EP0();      // Stall End Point 0
+             }
+          break;
+       default:                     // *** Invalid Command
+          if(DR_VendorCmnd())
+             EZUSB_STALL_EP0();            // Stall End Point 0
+          break;
+    }
+
 }
