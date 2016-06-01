e7f2aecb373452ba8057567422a8bc5800949c4c
adhdengineer <adhdengineer@gmail.com>
2016-06-01 16:14:34 +0100

    Adding a function to print a byte as hex. just does the nibbles does not prepend 0x or anything.

--- libCore/i2c.c
+++ libCore/i2c.c
@@ -2,6 +2,26 @@
 #include "fx2regs.h"
 #include "syncdly.h"          // SYNCDELAY macro
 
+#include "i2c.h"
+unsigned char get_nibble_as_hex(unsigned char nibble);
+
+unsigned char i2c_print_byte_as_hex(unsigned char addr, unsigned char byte)
+{
+    char temp[3];
+    temp[0] = get_nibble_as_hex(byte >> 4);
+    temp[1] = get_nibble_as_hex(byte);
+    temp[2] = '\0';
+    return i2c_print_string(addr, temp);
+}
+
+unsigned char get_nibble_as_hex(unsigned char nibble)
+{
+    nibble &= 0xf;
+    if (nibble <= 9) return '0' + nibble;
+    if (nibble <= 0xf) return 'A' + nibble - 10;
+    return 'x';
+}
+
 unsigned char i2c_print_string(unsigned char addr, char* str)
 {
     while (I2CS & bmSTOP);
--- libCore/i2c.h
+++ libCore/i2c.h
@@ -3,5 +3,5 @@
 
 // Writes a string to the address. str must be null terminated.
 unsigned char i2c_print_string(unsigned char addr, char* str);
-
+unsigned char i2c_print_byte_as_hex(unsigned char addr, unsigned char data);
 #endif
