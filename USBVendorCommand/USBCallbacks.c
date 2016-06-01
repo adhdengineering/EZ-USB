dc710d353c0fc3dbcf6ed91b183e7a95a730703d
adhdengineer <adhdengineer@gmail.com>
2016-06-01 16:19:24 +0100

    Added a vendor request that lets us turn on or off the LEDs and get the state as well. Note, there was a gotcha with the Vendor OUT request. you have to clear the EP0 buffer first during the handling so the new data can come in or else it will be the data left in the buffer from the last usage.

--- USBVendorCommand/USBCallbacks.c
+++ USBVendorCommand/USBCallbacks.c
@@ -6,9 +6,11 @@ BYTE Configuration; // Current configuration
 BYTE AlternateSetting; // Alternate settings
 
 #include <i2c.h>
+#include "LEDs.h"
 
 #define VR_NAKALL_ON    0xD0
 #define VR_NAKALL_OFF   0xD1
+#define LED_VENDOR_REQUEST 0x01
 
 /// Called when a Get Descriptor command is received
 BOOL DR_GetDescriptor(void)
@@ -66,9 +68,28 @@ BOOL DR_SetFeature(void)
 BOOL DR_VendorCmnd(void)
 {
     BYTE tmp;
-    //i2c_print_string(0x8, "DR_VC\n");
+
     switch (SETUPDAT[1])
     {
+        case LED_VENDOR_REQUEST:
+            if ((SETUPDAT[0] & 0x80) == 0x80)
+            {
+                EP0BUF[0] = GetLEDState(SETUPDAT[2]);
+                i2c_print_byte_as_hex(0x8, EP0BUF[0]);
+                i2c_print_string(0x8, "g\n");
+                EP0BCH = 0;
+                EP0BCL = 1;
+            }
+            else
+            {
+                EP0BCH = 0;
+                EP0BCL = 0;
+                while(EP0CS & bmEPBUSY);
+                i2c_print_byte_as_hex(0x8, EP0BUF[0]);
+                i2c_print_string(0x8, "s\n");
+                SetLEDState(SETUPDAT[2], EP0BUF[0]);
+            }
+            break;
         case VR_NAKALL_ON:
             tmp = FIFORESET;
             tmp |= bmNAKALL;
