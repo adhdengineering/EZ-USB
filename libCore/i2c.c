67506db45eb888fc25f63e6e344e3130e81d70f5
adhdengineer <adhdengineer@gmail.com>
2016-11-30 12:03:05 +0000

    Fixing the i2c read register code. It was working ok when talking to an arduino but when talking to the CMOS image sensor it would read the first register then blow up and stop all i2c coms.

--- libCore/i2c.c
+++ libCore/i2c.c
@@ -5,9 +5,9 @@
 #include "i2c.h"
 
 char get_nibble_as_hex(unsigned char nibble);
-i2c_error i2c_write_address_to_device(unsigned char device, unsigned short addr);
+i2c_error i2c_write_address_to_device(unsigned char device, unsigned char addr);
 i2c_error i2c_write_byte(unsigned char data);
-i2c_error i2c_read_byte(unsigned char *data);
+i2c_error i2c_read_byte(unsigned char *data, int bytesLeftAfterThisOne);
 void i2c_stop();
 
 
@@ -53,49 +53,20 @@ i2c_error i2c_print_string(unsigned char addr, char* str)
     return 1;
 }
 
-i2c_error i2c_write(unsigned char device, unsigned short addr, unsigned char* data, unsigned char len)
-{
-    unsigned char ret = 1;
-    while (I2CS & bmSTOP);
-
-    do
-    {
-        if (i2c_write_address_to_device(device, addr) == 0) { ret = 0; break; }
-
-        while (len)
-        {
-            if (i2c_write_byte(*(data++)) == 0) { ret = 0; break; }
-            len--;
-        };
-
-    } while(0);
-
-    i2c_stop();
-
-    return ret;
-}
-
-i2c_error i2c_read(unsigned char device, unsigned short addr, unsigned char* dst, unsigned char len)
+i2c_error i2c_write(unsigned char device, unsigned char reg, unsigned char* data, unsigned char len)
 {
     i2c_error ret = i2c_ok;
     while (I2CS & bmSTOP);
 
     do
     {
-        if ((ret = i2c_write_address_to_device(device, addr)) != i2c_ok) {  break; }
+        if ((ret = i2c_write_address_to_device(device, reg)) != i2c_ok) { break; }
 
-        I2CS |= bmSTART;
-        I2DAT = (device << 1) | 0x01;
-        while (!(I2CS & bmDONE));
-        if (I2CS & bmBERR) return i2c_error_flag;
-        if ((I2CS & bmACK) == 0) { ret = i2c_no_device_read_ack; break; }
-
-        *dst = I2DAT;
-        while (len)
+        while (len--)
         {
-            if ((ret = i2c_read_byte(dst++)) != i2c_ok) { break; }
-            len--;
+            if ((ret = i2c_write_byte(*(data++))) != i2c_ok) { break; }
         };
+
     } while(0);
 
     i2c_stop();
@@ -103,7 +74,53 @@ i2c_error i2c_read(unsigned char device, unsigned short addr, unsigned char* dst
     return ret;
 }
 
-i2c_error i2c_write_address_to_device(unsigned char device, unsigned short addr)
+// From Section 13.5.4 of the FX2LP Technical Reference Manual.
+//1. Set START=1. If BERR = 1, start timer. The timeout should be at least as long as the longest expected Start-to-Stop interval on the bus.
+//2. Write the 7-bit peripheral address and the direction bit (1 for a read) to I2DAT.
+//3. Wait for DONE=1 or for timer to expire*. If BERR=1, go to step 1.
+//4. If ACK=0, set STOP=1 and go to step 15.
+//5. Read I2DAT to initiate the first burst of nine SCL pulses to clock in the first byte from the slave. Discard the value that was read from I2DAT.
+//6. Wait for DONE=1. If BERR=1, go to step 1.
+//7. Read the just-received byte of data from I2DAT. This read also initiates the next read transfer.
+//8. Repeat steps 6 and 7 for each byte until ready to read the second-to-last byte.
+//9. Wait for DONE=1. If BERR=1, go to step 1.
+//10. Before reading the second-to-last I2DAT byte, set LASTRD=1.
+//11. Read the second-to-last byte from I2DAT. With LASTRD=1, this initiates the final byte read on the bus.
+//12. Wait for DONE=1. If BERR=1, go to step 1.
+//13. Set STOP=1.
+//14. Read the final byte from I2DAT immediately (the next instruction) after setting the STOP bit. By reading I2DAT while the
+    //‘stop’ condition is being generated, the just received data byte is retrieved without initiating an extra read transaction
+    //(nine more SCL pulses) on the I2Cbus.
+//15. Wait for STOP = 0 before initiating another transfer.
+i2c_error i2c_read(unsigned char device, unsigned char reg, unsigned char* dst, unsigned char len)
+{
+    i2c_error ret = i2c_ok;
+    while (I2CS & bmSTOP);
+    do
+    {
+        unsigned char discard;
+        if ((ret = i2c_write_address_to_device(device, reg)) != i2c_ok) {  break; }
+
+        I2CS |= bmSTART;
+        I2DAT = (device << 1) | 0x01;
+        while (!(I2CS & bmDONE));
+        if (I2CS & bmBERR) return i2c_error_flag;
+        if ((I2CS & bmACK) == 0) { ret = i2c_no_device_read_address_ack; break; }
+
+        discard = I2DAT;
+        while (len--)
+        {
+            if ((ret = i2c_read_byte(dst++, len)) != i2c_ok) { break; }
+        };
+        return i2c_ok;
+    } while(0);
+
+    i2c_stop();
+
+    return ret;
+}
+
+i2c_error i2c_write_address_to_device(unsigned char device, unsigned char addr)
 {
     i2c_error ret = i2c_ok;
     I2CS |= bmSTART;
@@ -111,10 +128,9 @@ i2c_error i2c_write_address_to_device(unsigned char device, unsigned short addr)
 
     while (!(I2CS & bmDONE));
     if (I2CS & bmBERR) return i2c_error_flag;
-    if ((I2CS & bmACK) == 0) return i2c_no_device_write_ack;
+    if ((I2CS & bmACK) == 0) return i2c_no_device_write_address_ack;
 
-    if ((ret = i2c_write_byte(addr & 0xff)) != i2c_ok) return ret;
-    if ((ret = i2c_write_byte((addr >> 8) & 0xff)) != i2c_ok) return ret;
+    if ((ret = i2c_write_byte(addr)) != i2c_ok) return ret;
 
     return i2c_ok;
 }
@@ -124,14 +140,16 @@ i2c_error i2c_write_byte(unsigned char data)
     I2DAT = data;
     while (!(I2CS & bmDONE));
     if (I2CS & bmBERR) return i2c_error_flag;
-    if ((I2CS & bmACK) == 0) return i2c_no_ack;
+    if ((I2CS & bmACK) == 0) return i2c_no_device_write_data_ack;
     return i2c_ok;
 }
 
-i2c_error i2c_read_byte(unsigned char *data)
+i2c_error i2c_read_byte(unsigned char *data, int bytesLeftAfterThisOne)
 {
     while (!(I2CS & bmDONE));
     if (I2CS & bmBERR) return i2c_error_flag;
+    if (bytesLeftAfterThisOne == 1)  I2CS |= bmLASTRD;
+    if (bytesLeftAfterThisOne == 0)  I2CS |= bmSTOP;
     *data = I2DAT;
     return i2c_ok;
 }
--- libCore/i2c.h
+++ libCore/i2c.h
@@ -5,14 +5,20 @@ typedef enum
 {
     i2c_ok = 0,
     i2c_error_flag,
-    i2c_no_device_write_ack,
-    i2c_no_device_read_ack,
-    i2c_no_ack,
+    i2c_no_device_write_address_ack,
+    i2c_no_device_write_data_ack,
+    i2c_no_device_read_address_ack,
+    i2c_no_device_read_data_ack,
+    i2c_error_0 = 0x20,
+    i2c_error_1 = 0x11,
+    i2c_error_2 = 0x12,
+    i2c_error_3 = 0x13,
+    i2c_error_4 = 0x14,
 }i2c_error;
 
 // Writes a string to the address. str must be null terminated.
 i2c_error i2c_print_string(unsigned char addr, char* str);
 i2c_error i2c_print_byte_as_hex(unsigned char addr, unsigned char data);
-i2c_error i2c_write(unsigned char device, unsigned short addr, unsigned char* data, unsigned char len);
-i2c_error i2c_read(unsigned char device, unsigned short addr, unsigned char* dst, unsigned char len);
+i2c_error i2c_write(unsigned char device, unsigned char reg, unsigned char* data, unsigned char len);
+i2c_error i2c_read(unsigned char device, unsigned char reg, unsigned char* dst, unsigned char len);
 #endif
